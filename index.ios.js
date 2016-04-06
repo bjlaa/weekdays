6b50cb1aa90d6c8acc1b57a75be6fb171f4903c6
Benjamin <benjamin.j.arias@gmail.com>
2016-04-06 21:40:12 +0200

    feat: Displayed days + added moment to get current day

--- index.ios.js
+++ index.ios.js
@@ -1,29 +1,44 @@
 //Import some code we need
 
 var React = require('react-native');
+var Moment = require('moment');
 var AppRegistry = React.AppRegistry;
 var View = React.View;
 var Text = React.Text;
 var StyleSheet = React.StyleSheet;
+var DayItem = require('./src/day-item');
 
 
 //Create a React component
 
 var Weekdays2 = React.createClass({
+	days: function() {
+		var daysItems = [];
+
+		for(var i=0; i<7; i++) {
+			var day = Moment().add(i, 'days').format('dddd');
+			daysItems.push(
+				<DayItem key={i} day={day} daysUntil={i} />
+			)
+		}
+		return daysItems;
+	},
 	render: function() {
 		return <View style={styles.container}>
 			<Text>
-				Days of the Week
+				Days of the Glawie:
 			</Text>
+			{this.days()}
 		</View>
 	}
 });
 
-//Style the React Component
+//Style the React component
 
 var styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center'
 	}
